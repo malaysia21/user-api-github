18ac8220292a01c8cd2e2893115a03ab0de8c271
unknown <agnieszka.puzia@gmail.com>
2021-05-09 22:33:33 +0200

    junit test + refactor

--- src/main/resources/sql/init.sql
+++ src/main/resources/sql/init.sql
@@ -1,29 +1,29 @@
 CREATE SCHEMA IF NOT EXISTS users;
 
-CREATE TABLE IF NOT EXISTS users.login_req_counter (
-  login varchar(250) NOT NULL PRIMARY KEY,
-  req_counter bigint NOT NULL
+CREATE TABLE IF NOT EXISTS users.login_request_count (
+  login varchar(255) NOT NULL PRIMARY KEY,
+  request_count bigint NOT NULL
 );
 
-CREATE OR REPLACE FUNCTION users.update_req_counter(inlogin character varying, OUT outReqCounter bigint)
+CREATE OR REPLACE FUNCTION users.update_request_count(inlogin character varying, OUT outReqCount bigint)
  RETURNS bigint
  LANGUAGE plpgsql
 AS $function$
-DECLARE 
-currentReqCounter bigint;
+DECLARE
+currentReqCount bigint;
 
 BEGIN
-	
-	SELECT req_counter INTO currentReqCounter FROM users.login_req_counter WHERE login = inlogin; 
-	
-	IF(currentReqCounter IS NULL) THEN
-		outReqCounter = 1;
-		INSERT INTO users.login_req_counter(login, req_counter)
-		VALUES (inlogin, outReqCounter);
-	ELSE 
-		outReqCounter = 1 + currentReqCounter;
-		UPDATE users.login_req_counter 
-		SET req_counter = outReqCounter
+
+	SELECT request_count INTO currentReqCount FROM users.login_request_count WHERE login = inlogin;
+
+	IF(currentReqCount IS NULL) THEN
+		outReqCount = 1;
+		INSERT INTO users.login_request_count(login, request_count)
+		VALUES (inlogin, outReqCount);
+	ELSE
+		outReqCount = 1 + currentReqCount;
+		UPDATE users.login_request_count
+		SET request_count = outReqCount
 		where login = inlogin;
 	END IF;
 
